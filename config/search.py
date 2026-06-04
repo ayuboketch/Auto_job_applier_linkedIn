a1972e2bc582518ab208fd6b38bfa44284ef7bed
ayuboketch84 <ayuboketch84@gmail.com>
2026-06-04 17:01:12 +0300

    Update

--- config/search.py
+++ config/search.py
@@ -19,10 +19,10 @@ version:    26.01.20.5.08
 
 # These Sentences are Searched in LinkedIn
 # Enter your search terms inside '[ ]' with quotes ' "searching title" ' for each search followed by comma ', ' Eg: ["Software Engineer", "Software Developer", "Selenium Developer"]
-search_terms = ["Frontend Developer", "React Developer", "Javascript Developer", "Front End Developer", "Web Developer", "Nodejs Developer"]
+search_terms = ["Frontend Developer", "React Developer", "Javascript Developer", "Front End Developer", "Web Developer", "junior frontend Developer"]
 
 # Search location, this will be filled in "City, state, or zip code" search box. If left empty as "", tool will not fill it.
-search_location = "United States"             # Some valid examples: "", "United States", "India", "Chicago, Illinois, United States", "90001, Los Angeles, California, United States", "Bengaluru, Karnataka, India", etc.
+search_location = "EMEA"           # Some valid examples: "", "United States", "India", "Chicago, Illinois, United States", "90001, Los Angeles, California, United States", "Bengaluru, Karnataka, India", etc.
 
 # After how many number of applications in current search should the bot switch to next search? 
 switch_number = 10                 # Only numbers greater than 0... Don't put in quotes
